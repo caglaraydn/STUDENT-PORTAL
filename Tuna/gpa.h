0d78e2089608877f3f391724807161b1a8fd48fc
Tuna Zeyneloğlu <99867102+TunaZeyneloglu@users.noreply.github.com>
2022-12-17 19:05:52 +0300

    Add files via upload

---
+++ Tuna/gpa.h
@@ -0,0 +1,70 @@
+#include <iostream>
+#include <string>
+#include <new>
+using namespace std;
+
+void calculateGPA()
+{
+    int course;
+    cout << "How many course will you enter? : ";
+    cin >> course;
+    cout << endl;
+    
+    double* point = new double[course];
+    double* credit = new double[course];
+    
+    for(int i=0; i<course; i++)
+    {
+        do{
+        cout << "Course " << i+1 << " point: ";
+        cin >> point[i];
+        cout << "Course " << i+1 << " credit: ";
+        cin >> credit[i];
+        cout << endl;
+        }while(point[i]<0 || point[i]>100 || credit[i]<1 ||credit[i]>4);
+    }
+    
+    double totCre = 0;
+    double mult;
+    double sumofmult = 0;
+    
+    for(int j=0; j<course; j++)
+    {
+
+        if(point[j]>89)
+            point[j] = 4;
+        
+        else if(point[j]>84)
+            point[j] = 3.5;
+        
+        else if(point[j]>79)
+            point[j] = 3;
+
+        else if(point[j]>74)
+            point[j] = 2.5;
+        
+        else if(point[j]>69)
+            point[j] = 2;
+        
+        else if(point[j]>64)
+            point[j] = 1.5;
+        
+        else if(point[j]>59)
+            point[j] = 1;
+        
+        else if(point[j]>49)
+            point[j] = 0.5;
+
+        else
+            point[j] = 0;
+
+
+        totCre += credit[j];
+        mult = credit[j]*point[j];
+        sumofmult += mult;
+        
+    }
+    
+    cout << "Total GPA: " << sumofmult/totCre << endl;
+    
+}
